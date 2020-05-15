7deebf2c7d57b82ead5aba8f72e95c5f710c490d
Nikita Ionov <ionov.goo@gmail.com>
2020-05-15 20:06:21 +0300

    fixes for require global

--- src/docopt.php
+++ src/docopt.php
@@ -5,7 +5,14 @@ use Docopt;
 
 function runDocopt($doc)
 {
-    require('vendor/docopt/docopt/src/docopt.php');
+    $pathLoc = __DIR__ . '/../vendor/docopt/docopt/src/docopt.php';
+    $pathGlob = __DIR__ . '/../../../docopt/docopt/src/docopt.php';
+    if (file_exists($pathGlob)) {
+        require_once $pathGlob;
+    } else {
+        require_once $pathLoc;
+    }
+    // require('vendor/docopt/docopt/src/docopt.php');
     $args = Docopt::handle($doc, array('version'=>'Find different 0.1'));
     foreach ($args as $k=>$v)
         echo $k.': '.json_encode($v).PHP_EOL;
